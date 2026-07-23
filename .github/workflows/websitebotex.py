b24ef4f3d4f382813dc9170dc6ab1806abd06e1f
humanmade21 <joeycom@tutamail.com>
2026-07-23 17:01:59 +0300

    Update websitebotex.py

--- .github/workflows/websitebotex.py
+++ .github/workflows/websitebotex.py
@@ -36,7 +36,8 @@ options.add_argument(f"--user-data-dir={USER_DATA_DIR}")
 options.add_argument("--profile-directory=Default")
 
 options.binary_location = OPERA_GX_PATH
-options.add_argument("--headless=new")
+options.add_argument("--headless")
+options.add_argument("--remote-debugging-pipe")
 options.add_argument("window-size=1920x1080")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
